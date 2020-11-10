7c3ad351bd859786dcd9a6411813a2f2d3c7642e
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-10 09:51:02 +0100

    Refactoring
    - print new student's data
    - refactoring of JavaScript code

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ var student = {
   'nome': 'Alessandro',
   'cognome': 'Ferraro',
   'età': 28
-}
+};
 
-document.write('<h1>Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.</h1>')
+document.write('<h1>Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.</h1>');
 
 //Stampare a schermo attraverso il for in tutte le proprietà.
 for (var key in student){
-  document.write('<p>La proprietà '+key+' dello studente è: '+student[key]);
+  document.write('<p>La proprietà '+key+' dello studente è: '+student[key]+'</p>');
 }
 
 //Creare un array di oggetti di studenti.
@@ -36,17 +36,14 @@ var students = [
     'cognome': 'Bianchi',
     'età': 35
   }
-]
+];
 
-document.write('<h1>Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome</h1>')
+document.write('<h1>Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome</h1>');
 
 //Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 for(var i=0; i<students.length;i++){
-  for(var key in students[i]){
-    if((key == 'nome') || (key == 'cognome')){
-    document.write('<p>La proprietà '+key+' dello studente numero '+(i+1)+' è: '+students[i][key]);
-    }
-  }
+  document.write('<p>Il nome dello studente numero '+(i+1)+' è: '+students[i]['nome']+'</p>');
+  document.write('<p>Il cognome dello studente numero '+(i+1)+' è: '+students[i]['cognome']+'</p>');
 }
 
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
@@ -54,7 +51,7 @@ var newStudent = {
   'nome': undefined,
   'cognome': undefined,
   'età': undefined
-}
+};
 
 newStudent.nome = prompt('Inserisci il nome');
 newStudent.cognome = prompt('Inserisci il cognome');
@@ -65,4 +62,8 @@ console.log(newStudent);
 students.push(newStudent);
 
 console.log(students);
-//I dati del nuovo studente inserito dall'utente non verranno stampati in quanto il for che procede alla stampa è antecedente ai prompt che inseriscono il nuovo studente
+
+document.write('<h1>Lo studente aggiunto dall\'utente è:</h1>');
+for (var key in students[3]){
+  document.write('<p>'+key+': '+students[3][key]+'</p>');
+}
